19456d7848a417a0054305588cfab27424df0a98
Satwikan <satwikanmol25@gmail.com>
2021-06-21 14:40:43 +0530

    trying to remove cors error

--- frontUrl.js
+++ frontUrl.js
@@ -1,2 +1,2 @@
 module.exports = "https://parkx.netlify.app/";
-// https://parkx.netlify.app/
+// module.exports = "https://localhost:3000/"
--- server.js
+++ server.js
@@ -15,31 +15,14 @@ app.use(
 app.use(express.json());
 
 // CORS
-app.use(cors());
+// app.use(cors());
 
-// Add headers
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", FRONT_URL);
-
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-
-//   // Pass to next layer of middleware
-//   next();
-// });
+app.options('/api', function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.end();
+});
 
 // DB Config
 const db = require("./keys").mongoURI;
