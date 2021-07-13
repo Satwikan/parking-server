897f0460d3fb95ca75a508088b79d50ed309e3f0
Satwikan <69719072+Satwikan@users.noreply.github.com>
2021-07-13 11:52:49 +0530

    Delete keys.js

--- keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Satwikan:satwikanKaPassword@cluster0.rslop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-  user: "rengoku.flame.user@gmail.com",
-  pass: "rengokuFlameHashira",
-};
